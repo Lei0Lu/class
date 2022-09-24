abe76654cd4bd9ecde9d858f2787ce9074e22db8
Lei0Lu <9.lei.ly.9@gmail.com>
2022-09-24 10:45:04 +0300

    first commit

---
+++ 27.09.2022/File.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _27._09._2022
+{
+    internal class File
+    {
+        public string Name { get; set; }
+        public FileExtension Extension { get; set; }
+        public double Size { get; set; }
+        public DateTime Datetime { get; set; }
+
+        public File(string name, FileExtension extension, double size, DateTime dateTime)
+        {
+            Name = name;
+            Extension = extension;
+            Size = size;
+            Datetime = dateTime;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name}.{Extension}  {Size}  {Datetime}";
+        }
+
+
+
+        public string Sizeselection()
+        {
+
+            if (Size < 1000)
+            {
+                return $"{Size} байт";
+            }
+            if (Size >= 1000 || Size < 1000000)
+            {
+
+                return $"{Size / 1024} килобайт";
+            }
+            if (Size >= 1000000)
+            {
+
+                return $"{Size / 1048576} мегабайт";
+            }
+            else
+            {
+                return "ошибка";
+            }
+
+        }
+        public enum FileExtension
+        {
+
+            txt,
+            rar,
+            pdf,
+            docx,
+
+
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+}
---
+++ 27.09.2022/Program.cs
@@ -0,0 +1,37 @@
+﻿
+
+namespace _27._09._2022;
+
+
+
+File[] files = new File[]
+{
+    new File("HHf", File.FileExtension.pdf, 50084, new DateTime(2009, 05, 30) ),
+    new File("file", File.FileExtension.rar, 5342, new DateTime(2021, 07, 27) ),
+    new File("file(1)", File.FileExtension.pdf, 1282337, new DateTime(2001, 03, 1) ),
+    new File("filefile", File.FileExtension.txt, 16, new DateTime(2015, 12, 29) ),
+    new File("newfile", File.FileExtension.pdf, 2281337, new DateTime(2022, 01, 5) )
+
+};
+
+
+foreach (File item in files)
+  {
+      if(Item.Extension == FileExtension.txt){
+
+      ToString();
+        }
+        else
+
+}
+
+
+
+foreach (File item in files)
+{
+    if(DateTime<= DateTime(2022,09, 17))
+    {
+
+
+    }
+}
